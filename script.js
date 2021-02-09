419f03ba1f4eee7ce72997f7e7f2161797045206
Mr. Mueller <bmueller@hstat.org>
2021-02-09 14:19:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// the first two numbers in the fibonacci sequence
+var fibs = [0,1];
 
+// how many total fibonacci numbers we eventually want in the array
+var numOfFibs = 10;
+
+// replace all of the SOMETHINGs with the correct code
+for(var i = SOMETHING; i < SOMETHING; i++) {
+  var num1 = SOMETHING;
+  var num2 = SOMETHING
+  var newFib = num1 + num2;
+  fibs.push(newFib);
+}
+
+console.log(fibs.length); // 10
+console.log(fibs); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+// EXTENSION:
+// Let the user decide how many fibonacci numbers to make
+// And/or think of what other fun things you could do with this!
